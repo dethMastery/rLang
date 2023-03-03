82187ae96ad253ccad88b16fe79c484fc59b5ce7
dethMastery <me@suphakit.net>
2023-03-03 15:48:19 +0700

    feat: add 02

---
+++ 02/file.R
@@ -0,0 +1,24 @@
+x <- c(20, 44, 30, 67, 47, 12, 46, 44, 50, 80) # มีทั้งหมด 10 ตัว
+y <- 25
+z <- -25
+i <- 1.4
+
+max(x)
+
+min(x)
+
+median(x)
+
+mean(x)
+
+sum(x)
+
+sqrt(y)
+
+abs(z)
+
+ceiling(i)
+
+floor(i)
+
+cos(i)
